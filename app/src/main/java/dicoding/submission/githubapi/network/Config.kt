931c8b73d6dd001aa5b4fa34c5e406eb4f318360
ahmadsyahrullft9 <ahmadsyahrullft9@gmail.com>
2020-07-02 08:10:56 +0700

    memperbarui gitignore

--- app/src/main/java/dicoding/submission/githubapi/network/Config.kt
+++ app/src/main/java/dicoding/submission/githubapi/network/Config.kt
@@ -1,6 +1,6 @@
 package dicoding.submission.githubapi.network
 
 object Config {
-    val GITHUB_API_KEY: String = "token 0f392e1bd1646d58dd8c699dc893d7258e1ca745" //your github api token
+    val GITHUB_API_KEY: String = "token xxxx" //your github api token
     val BASE_URL: String = "https://api.github.com/"
 }
